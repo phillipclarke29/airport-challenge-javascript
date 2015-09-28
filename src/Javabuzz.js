0da9259bcee5b11e693edccab4f9c0b40aac2e82
Proskurina <yproskurina@yahoo.com>
2015-09-28 17:21:43 +0100

    Added says function, works with 'Java' and 'Buzz'

--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,9 +1,34 @@
 var Javabuzz = function() {};
 
 
-Javabuzz.prototype.isDivisibleByThree = function(number) {
-  return (number % 3 === 0);
-
-
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
 
 };
+
+Javabuzz.prototype.isDivisibleByThree = function(number) {
+  return this._isDivisibleBy(number, 3);
+
+};
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  return this._isDivisibleBy(number, 5);
+
+};
+
+Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number, 15);
+
+};
+
+Javabuzz.prototype.says = function(number) {
+  if (this.isDivisibleByThree(number)) {
+    return "Java";
+  }
+  else if (this.isDivisibleByFive(number)) {
+    return "Buzz";
+  };
+
+};
+
+console.log(Javabuzz.says());
