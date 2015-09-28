f10fd86c533786efa042943aa4d5b71e775132c4
philip <philip.clarke@redhood.co.uk>
2015-09-28 16:40:44 +0100

    6 initial tests added

---
+++ spec/JavabuzzSpec.js
@@ -0,0 +1,45 @@
+describe('Javabuzz', function() {
+
+  var javabuzz;
+
+  beforeEach(function() {
+
+    javabuzz = new Javabuzz();
+  });
+
+
+  describe('knows when a number is', function() {
+
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
+  });
+
+  describe('knows when a number NOT', function() {
+
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(3)).toBe(false);
+    });
+
+    it('divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(13)).toBe(false);
+    });
+
+  });
+
+
+
+});
---
+++ src/Javabuzz.js
@@ -0,0 +1,9 @@
+var Javabuzz = function() {};
+
+
+Javabuzz.prototype.isDivisibleByThree = function(number) {
+  return (number % 3 === 0);
+
+
+
+};
